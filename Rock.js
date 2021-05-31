7e8903bd635726e87f2381e20c3a0550cf988f8c
Kripa Gupta <kripagupta.697@gmail.com>
2021-05-31 16:01:25 +0530

    created Crash tram simulation

---
+++ Rock.js
@@ -0,0 +1,17 @@
+class Rock {
+    constructor (x,y,width,height){
+        var options = {
+            isStatic : false
+        }
+        this.body=Bodies.rectangle(x,y,width,height,options);
+        this.height=height;
+        this.width=width;
+        this.image=loadImage("images/rock1.png");
+        World.add(myWorld,this.body);
+    }
+    show(){
+        var pos = this.body.position
+        imageMode(CENTER);
+        image(this.image,pos.x,pos.y,this.width,this.height);
+    }
+}
